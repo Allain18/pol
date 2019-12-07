fccd652c0c0a1095910765c53af3f91f17ed58b9
Alain <alaingirardvd@gmail.com>
2019-12-06 18:55:29 -1000

    Switch to class Add check_stack, plus, print Make main as loop

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,11 +1,58 @@
 """Calculator using the reverse polish notation"""
 
 
-def main():
-    """Fonction principale"""
+class Calculator:
+    """Class Calculator"""
 
-    print("Reverse polish notation calculator")
+    def __init__(self):
+        self.stack = []
+
+        self.loop = True
+
+        self.operation = {
+            "+": self.plus,
+            ".": self.print
+        }
+
+    def main(self):
+        """Fonction principale"""
+
+        print("Reverse polish notation calculator")
+
+        try:
+            while self.loop:
+                data = input(">")
+
+                for i in data.split():
+                    try:
+                        i = int(i)
+                        self.stack.append(i)
+                    except ValueError:
+                        self.operation[i]()
+
+        except KeyboardInterrupt:
+            pass
+
+    def check_stack(self, num):
+        """Check if enough number are in the stack"""
+        if len(self.stack) < num:
+            return False
+
+        return True
+
+    def plus(self):
+        """Take 2 number from the stack, add them and put the result in the stack"""
+        if self.check_stack(2):
+            value1 = self.stack.pop()
+            value2 = self.stack.pop()
+            self.stack.append(value1 + value2)
+
+    def print(self):
+        """Take one number from the stack and print it"""
+        if self.check_stack(1):
+            print("{}".format(self.stack.pop()))
 
 
 if __name__ == "__main__":
-    main()
+    CAL = Calculator()
+    CAL.main()
