3e9cb0f16d41adc73040b762b55e88dda1824c9d
Alain <alaingirardvd@gmail.com>
2019-12-06 18:07:19 -1000

    Print what is it

---
+++ calculator/calculator.py
@@ -0,0 +1,11 @@
+"""Calculator using the reverse polish notation"""
+
+
+def main():
+    """Fonction principale"""
+
+    print("Reverse polish notation calculator")
+
+
+if __name__ == "__main__":
+    main()
