37ff835d5ac614ed20a694d19d1bcef983c6709b
Alain <alaingirardvd@gmail.com>
2019-12-18 17:26:18 +1100

    get_command Small program for updating readme with commands of the calculator

---
+++ get_commands.py
@@ -0,0 +1,23 @@
+"""Update README with commands"""
+
+import calculator
+
+
+def main():
+    """Update readme with command"""
+
+    doc = ""
+    for command, method in calculator.Calculator().operation.items():
+        doc += "__\"{}\"__: {}\n\n".format(command, method.__doc__)
+
+    with open("README.md", "r") as readme:
+        list_readme = readme.read().split("## List of commands\n")
+
+    list_readme[1] = doc + list_readme[1]
+
+    with open("README.md", "w") as readme:
+        readme.write("## List of commands\n".join(list_readme))
+
+
+if __name__ == "__main__":
+    main()
